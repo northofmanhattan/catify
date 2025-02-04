46b7ebcd11513c85ce7ef408d6b63f9789015a18
James Hoffman <james.hoffman@aero.org>
2025-02-03 21:13:59 -0800

    Applying local changes

--- script.js
+++ script.js
@@ -6,14 +6,6 @@ dragElement(document.getElementById('face3'));
 dragElement(document.getElementById('face4'));
 dragElement(document.getElementById('face5'));
 dragElement(document.getElementById('face6'));
-dragElement(document.getElementById('face7'));
-dragElement(document.getElementById('face8'));
-dragElement(document.getElementById('face9'));
-dragElement(document.getElementById('face10'));
-dragElement(document.getElementById('face11'));
-dragElement(document.getElementById('face12'));
-dragElement(document.getElementById('face13'));
-dragElement(document.getElementById('face14'));
 
 function dragElement(terrariumElement) {
 	let xStored=0, yStored=0, xCalced=0, yCalced=0;
@@ -25,6 +17,7 @@ function dragElement(terrariumElement) {
 	  yStored = event.clientY; // store initial Y value
 	  document.onpointermove = calcDrag; // calculates move (called continuously)
 	  document.onpointerup = stopDrag; // stops dragging process
+	  
 	}
 	function calcDrag(event) {
 	  xCalced = xStored - event.clientX; // calculates how far cursor has moved
@@ -34,6 +27,8 @@ function dragElement(terrariumElement) {
 	  // Moving the element using CSS
 	  terrariumElement.style.left = (terrariumElement.offsetLeft - xCalced) + 'px';
 	  terrariumElement.style.top = (terrariumElement.offsetTop - yCalced) + 'px';
+	  terrariumElement.style.position = "absolute";
+
 	}
 	function stopDrag () {
 	  document.onpointerup = null;   // removes event listener
